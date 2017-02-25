d05bfd56da4ac0895f37280ec39fbadd29b92526
Andrea K <shimmeringbluorb@gmail.com>
2017-02-25 11:17:30 -0600

    chapter 11

--- angular.js
+++ angular.js
@@ -1,2 +1,1 @@
-<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.0/angular.min.js">
-</script>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.0/angular.min.js"></script>
